ae52cf5ad63de13bd00a55a8bf6e7ef2ec6c2d6b
Sailesh <Sailesh@user-PC.medecare.inc>
2018-03-22 09:00:06 -0400

    Hello Committed

---
+++ facebook.com/src/main/java/facebook/com/offer.java
@@ -0,0 +1,23 @@
+package facebook.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class offer {
+
+	WebDriver driver;
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Library\\chromedriver_win32\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+driver.get("http://cognextech.com/");
+Actions a = new Actions(driver);
+a.moveToElement(driver.findElement(By.linkText("IT Course"))).build().perform();
+	}
+
+}
---
+++ facebook.com/src/test/java/facebook/com/TV.java
@@ -0,0 +1,11 @@
+package facebook.com;
+
+import org.testng.annotations.Test;
+
+public class TV {
+@Test
+
+public void sailesh(){
+	System.out.println("Hello bitch");
+}
+}
